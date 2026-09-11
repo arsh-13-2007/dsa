ef93d3a8087af8cc5dbe18669aeaba78b9a00ec5
arsh <arsh40896@gmail.com>
2026-09-11 23:17:55 +0530

    stack_start

---
+++ 100days_dsa/recursion_rewise.cpp
@@ -0,0 +1,18 @@
+#include<iostream> 
+using namespace std ; 
+
+vector 
+int main (){
+    int n ;
+    cout<<"enter number of elements in array : " ; 
+    cin>>n ; 
+    int camparison = 0 ; 
+    vector<int>arr(n) ; 
+   cout<<"enter values in array :" ; 
+   for( int i = 0 ; i < n ; i ++){
+    cin>>arr[i] ; 
+   }
+
+    reverse(arr , n )
+
+}
---
+++ stack/stack_implementation_vector.cpp
@@ -0,0 +1,35 @@
+// stack implementation using vector 
+// lifo ( last in first out)
+
+#include<iostream>
+#include<vector>
+using namespace std ; 
+class stack {
+    vector<int>v ; 
+public:
+   void push(int val){
+    v.push_back(val) ;
+   }
+   void pop(){
+    v.pop_back() ; 
+   }
+   int top(){
+    return v[v.size() -1] ; 
+   }
+   bool empty(){
+      return v.size() == 0 ;
+   }
+};
+int main(){
+   stack s ; 
+   s.push(10) ;
+   s.push(20) ;
+   s.push(30) ;
+   s.push(40) ;
+   while(!s.empty()){
+    cout<<s.top()<<" " ; 
+    s.pop() ; 
+   }
+   cout<< endl<<"done" ; 
+   return 0 ; 
+}
